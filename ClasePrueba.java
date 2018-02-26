294f019a2313ff1e908267125c303206e52fedb8
Juan <juanpilenoo@gmail.com>
2018-02-26 14:00:49 +0100

    Añadido clase

---
+++ ClasePrueba.java
@@ -0,0 +1,28 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class ClasePrueba
+{
+     
+    private int x;
+
+    /**
+     * Constructor  
+     */
+    public ClasePrueba()
+    {
+         
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
